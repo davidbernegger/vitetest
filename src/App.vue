797ef764de21ad1e814af068a393d549b1f826a6
David Bernegger <david.bernegger@gmail.com>
2020-09-21 01:23:25 +0200

    test master

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,37 @@
 <template>
-  <div>Edit Me My man</div>
+  <div
+    v-for="(x, y, z) in object"
+    :key="z"
+    class="text"
+    @click="addPropAndChangeColor"
+  >
+    {{ x }} 
+  </div>
 </template>
 
-<script>
+<script setup>
+import { reactive, ref } from 'vue'
+const object = reactive({
+	firstProperty: 'This Has Color',
+	SecondProperty: 'This Also Has color'
+})
 
-export default {
-	name: 'App',
+const color = ref('red')
+
+const addPropAndChangeColor = function () {
+	object[Math.random()] = 'Added Property does not have color'
+	color.value = 'blue'
+}
+
+export {
+	object,
+	addPropAndChangeColor,
+	color
 }
 </script>
+
+<style vars="{ color }">
+.text {
+  color: var(--color);
+}
+</style>
